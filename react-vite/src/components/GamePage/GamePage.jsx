ca6cfc90b928dd9e74ced951f6a7e52798d75545
Matthew <matthewhung.shop@gmail.com>
2024-07-03 03:52:27 -0400

    key inputs added

--- react-vite/src/components/GamePage/GamePage.jsx
+++ react-vite/src/components/GamePage/GamePage.jsx
@@ -1,8 +1,23 @@
 import "./GamePage.css";
 import LeftPanel from "./LeftPanel";
+import RightPanel from "./RightPanel";
+import { useEffect } from "react";
 
 function GamePage() {
   const tiles = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  useEffect(() => {
+    const handleKey = (e) => {
+      const key = e.key;
+      console.log(key);
+    };
+    document.addEventListener("keydown", handleKey, true);
+
+    return () => {
+      document.removeEventListener("keydown", handleKey);
+    };
+  });
+
   return (
     <div className="game-page">
       <div id="left-panel">
@@ -14,7 +29,7 @@ function GamePage() {
         <div id="game-keys">
           {tiles.map((key) => {
             return (
-              <div className="key-tile" id={key}>
+              <div className="key-tile" key={key}>
                 {key}
               </div>
             );
@@ -22,7 +37,7 @@ function GamePage() {
         </div>
       </div>
       <div id="right-panel">
-        Right Panel<div id="level">Level</div>
+        <RightPanel />
       </div>
     </div>
   );
